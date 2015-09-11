cc026dd64cb2d1191c9abc18690a508e30250190
tracer-ics2015 <tracer@njuics.org>
2015-09-11 23:35:59 +0800

    >  compile NEMU 141220162 zhouyc Linux Debian 3.16.0-4-686-pae #1 SMP Debian 3.16.7-ckt11-1 (2015-05-24) i686 GNU/Linux  23:35:59 up  4:31,  4 users,  load average: 0.00, 0.04, 0.05 fe62031c104ebb0948b38e24f064ea034f86a56b

--- nemu/src/monitor/debug/ui.c
+++ nemu/src/monitor/debug/ui.c
@@ -40,7 +40,9 @@ static int cmd_q(char *args) {
 }
 
 static int cmd_si(char *args) {
-	cpu_exec(atoi(args));
+	if (args==NULL)
+		cpu_exec(1);
+	else	cpu_exec(atoi(args));
 	return 0;
 }
 
@@ -57,10 +59,10 @@ static int cmd_info(char *args) {
 		printf("eip\t\t%p\t0x%x\t%d\n",&cpu.eip,cpu.eip,cpu.eip);
 		int i;
 		for (i=0;i<8;i++)
-			printf("%s\t0x%x\n",regsw[i],cpu.gpr[i]._16);
+			printf("%s\t\t%p\t0x%x\t%d\n",regsw[i],&cpu.gpr[i]._16,cpu.gpr[i]._16,cpu.gpr[i]._16);
 		for (i=0;i<4;i++){
-			printf("%s\t0x%x\n",regsb[i],cpu.gpr[i]._8[0]);
-			printf("%s\t0x%x\n",regsb[i+4],cpu.gpr[i]._8[1]);
+			printf("%s\t\t%p\t0x%x\t%d\n",regsb[i],&cpu.gpr[i]._8[0],cpu.gpr[i]._8[0],cpu.gpr[i]._8[0]);
+			printf("%s\t\t%p\t0x%x\t%d\n",regsb[i+4],&cpu.gpr[i]._8[1],cpu.gpr[i]._8[1],cpu.gpr[i]._8[1]);
 		}
 	}
 	return 0;
