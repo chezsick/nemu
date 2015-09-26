d8753afd935835860b4187a93fa5219a65a63e03
tracer-ics2015 <tracer@njuics.org>
2015-09-26 19:00:24 +0800

    >  compile NEMU 141220162 zhouyc Linux Debian 3.16.0-4-686-pae #1 SMP Debian 3.16.7-ckt11-1 (2015-05-24) i686 GNU/Linux  19:00:24 up  4:22,  5 users,  load average: 0.01, 0.13, 0.12 f92e3e46beda7b27b88020a9ef5767c2742ff93b

--- nemu/src/monitor/debug/watchpoint.c
+++ nemu/src/monitor/debug/watchpoint.c
@@ -45,9 +45,10 @@ WP* new_wp(char* str,int res)
 		new_=p;
 		pre->next=NULL;
 	}
-	printf("**%s\n%d\n",str,res);
+	//printf("**%s\n%d\n",str,res);
 	strcpy(new_->exp,str);
 	new_->val=res;
+	printf("**%s\n%d\n",str,res);
 	return new_;
 }
 void free_wp(WP *wp)
