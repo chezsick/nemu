c8cca9ff510690b99bf203dfc67faaf183852336
tracer-ics2015 <tracer@njuics.org>
2015-09-26 23:06:54 +0800

    >  compile NEMU 141220162 zhouyc Linux Debian 3.16.0-4-686-pae #1 SMP Debian 3.16.7-ckt11-1 (2015-05-24) i686 GNU/Linux  23:06:54 up  8:29,  6 users,  load average: 0.00, 0.04, 0.05 4814304273c76344344677e1958d151e55991242

--- nemu/src/monitor/debug/watchpoint.c
+++ nemu/src/monitor/debug/watchpoint.c
@@ -80,9 +80,9 @@ void free_wp(int no)
 		}
 	}
 	if (p==head){
-		head->next=free_;
-		free_=head;
-		head=p->next;
+		head=head->next;
+		p->next=free_;
+		free_=p;
 	}else{
 		WP* pre=head;
 		while(pre->next!=p) pre=pre->next;
