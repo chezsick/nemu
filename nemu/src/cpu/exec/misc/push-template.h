666ad11391dc43b83efe51450f38cf55c6608fd6
tracer-ics2015 <tracer@njuics.org>
2015-10-12 20:02:10 +0800

    >  compile NEMU 141220162 zhouyc Linux Debian 3.16.0-4-686-pae #1 SMP Debian 3.16.7-ckt11-1 (2015-05-24) i686 GNU/Linux  20:02:10 up  2:16,  8 users,  load average: 0.00, 0.01, 0.05 6019ba4be75b8f4862803182450462173d42654c

--- nemu/src/cpu/exec/misc/push-template.h
+++ nemu/src/cpu/exec/misc/push-template.h
@@ -3,8 +3,10 @@
 #define instr push
 
 static void do_execute() {
-	cpu.esp-=4;
-
+	uint32_t n=4;
+	if (ops_decoded.is_data_size_16) n=2; 
+	cpu.esp-=n;
+	MEM_W(cpu.esp,op_src->val);
 	print_asm_template2();
 }
 make_instr_helper(rm)
