cc9f2574b0d9779b9f784f742240902f3b101156
tracer-ics2015 <tracer@njuics.org>
2015-10-13 20:01:26 +0800

    >  compile NEMU 141220162 zhouyc Linux Debian 3.16.0-4-686-pae #1 SMP Debian 3.16.7-ckt11-1 (2015-05-24) i686 GNU/Linux  20:01:26 up  3:10,  6 users,  load average: 0.03, 0.08, 0.06 98f8c56eff1a85cb4f5e613816147e5da22d12ec

--- nemu/src/cpu/exec/data-mov/push-template.h
+++ nemu/src/cpu/exec/data-mov/push-template.h
@@ -7,7 +7,7 @@ static void do_execute() {
 	if (ops_decoded.is_data_size_16) n=2; 
 	cpu.esp-=n;
 	MEM_W(cpu.esp,op_src->val);
-	print_asm_template2();
+	print_asm_template1();
 }
 make_instr_helper(rm)
 #include "cpu/exec/template-end.h"
--- nemu/src/cpu/exec/transfer/call-template.h
+++ nemu/src/cpu/exec/transfer/call-template.h
@@ -4,11 +4,11 @@
 
 static void do_execute() {
 	uint32_t n=4;
-	if (ops_decoded.is_data_size_16) n=2;
+	if (DATA_BYTE==2) n=2;
 	cpu.esp-=n;
 	MEM_W(cpu.esp,op_src->val);
 	cpu.eip+=op_src->val;	
-	print_asm_template2();
+	print_asm_template1();
 }
 
 make_instr_helper(i)
