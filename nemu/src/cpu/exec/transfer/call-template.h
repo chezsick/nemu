9b466499eac134c337add795cc3c05a9fadb8062
tracer-ics2015 <tracer@njuics.org>
2015-10-13 20:39:08 +0800

    >  compile NEMU 141220162 zhouyc Linux Debian 3.16.0-4-686-pae #1 SMP Debian 3.16.7-ckt11-1 (2015-05-24) i686 GNU/Linux  20:39:08 up  3:47,  8 users,  load average: 0.01, 0.03, 0.05 aac89fb4849f4f08cd407fec38ee0d7b525fe56

--- nemu/src/cpu/exec/transfer/call-template.h
+++ nemu/src/cpu/exec/transfer/call-template.h
@@ -6,9 +6,8 @@ static void do_execute() {
 	uint32_t n=4;
 	if (ops_decoded.is_data_size_16) n=2;
 	cpu.esp-=n;
-	printf("ok\n");
 	MEM_W(cpu.esp,op_src->val);
-	assert(0);
+	printf("ok\n");
 	cpu.eip+=op_src->val;	
 	print_asm_template1();
 }
