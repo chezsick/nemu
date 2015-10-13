048f43c21e78eb2a6b6ef55eeb1ef5857f609bef
tracer-ics2015 <tracer@njuics.org>
2015-10-14 00:08:20 +0800

    >  compile NEMU 141220162 zhouyc Linux Debian 3.16.0-4-686-pae #1 SMP Debian 3.16.7-ckt11-1 (2015-05-24) i686 GNU/Linux  00:08:20 up  6:44,  9 users,  load average: 0.00, 0.01, 0.05 54c0d41274de8e1595f88591bd3835aac9ccbc94

--- nemu/src/cpu/exec/transfer/jcc-template.h
+++ nemu/src/cpu/exec/transfer/jcc-template.h
@@ -1,13 +1,14 @@
 #include "cpu/exec/template-start.h"
 
-#define instr push
+#define instr je
 
 static void do_execute() {
-	uint32_t n=4;
-	if (ops_decoded.is_data_size_16) n=2; 
-	cpu.esp-=n;
-	MEM_W(cpu.esp,op_src->val);
-	print_asm_template1();
+	if (cpu.EFLAGS.ZF==0){
+		cpu.eip+=(int)op_src->val;
+		if (ops_decoded.is_data_size_16)
+			cpu.eip&=0x0000ffff;
+	}
+	print_asm_template2();
 }
 //make_instr_helper(i)
 make_instr_helper(r)
---
+++ nemu/src/cpu/exec/transfer/jcc.h
@@ -0,0 +1,10 @@
+#ifndef __JCC_H__
+#define __JCC_H__
+
+make_helper(je_r_v);
+/*
+make_helper(push_m_v);
+make_helper(push_i_v);
+*/
+make_helper(je_r_b);
+#endif
