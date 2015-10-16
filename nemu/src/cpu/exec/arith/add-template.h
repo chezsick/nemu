5488cfcec53f39f5e78c3227c0bb4b9a3ba1baa2
tracer-ics2015 <tracer@njuics.org>
2015-10-17 00:32:46 +0800

    >  compile NEMU 141220162 zhouyc Linux Debian 3.16.0-4-686-pae #1 SMP Debian 3.16.7-ckt11-1 (2015-05-24) i686 GNU/Linux  00:32:46 up  4:28,  9 users,  load average: 0.00, 0.01, 0.05 365126154c849bcfc911a256e95a6218e02e948d

---
+++ nemu/src/cpu/exec/arith/add-template.h
@@ -0,0 +1,58 @@
+#include "cpu/exec/template-start.h"
+
+#define instr add
+
+static void do_execute() {
+	DATA_TYPE result = op_dest->val + op_src->val;
+	OPERAND_W(op_dest, result);
+	DATA_TYPE p=result;
+	uint32_t i;
+	p=p&0xff;
+	p^=p>>4;
+	p^=p>>2;
+	p^=p>>1;
+	if (!!(result>>31))
+		cpu.EFLAGS.SF=1;
+	else 	
+		cpu.EFLAGS.SF=0;
+	if (result>op_src2->val)
+		cpu.EFLAGS.CF=1;
+	else   
+		cpu.EFLAGS.CF=0;
+	if (result==0) 
+		cpu.EFLAGS.ZF=1;
+	else 
+		cpu.EFLAGS.ZF=0;
+	if (p==0)
+		cpu.EFLAGS.PF=1;
+	else 
+		cpu.EFLAGS.PF=0;
+	//DATA_TYPE af=0;
+	for (i=0;i<4;i++){
+		DATA_TYPE a,b;
+		a=result>>(i*4)&0xf;
+		b=op_dest->val>>(i*4)&0xf;
+		if (a>b) 
+			cpu.EFLAGS.AF=1;
+		else 
+			cpu.EFLAGS.AF=0;
+
+	}
+	if ((op_src->val>0&&op_dest->val<0&&result>0)||(op_src->val<0&&op_dest->val>0&&result<0))
+		cpu.EFLAGS.OF=1;
+	else 	
+		cpu.EFLAGS.OF=0;
+	print_asm_template2();
+}
+
+#if DATA_BYTE==2 || DATA_BYTE==4
+make_instr_helper(si2rm)
+#endif
+
+make_instr_helper(i2a)
+make_instr_helper(i2rm)
+//make_instr_helper(si2rm)
+make_instr_helper(r2rm)
+make_instr_helper(rm2r)
+
+#include "cpu/exec/template-end.h"
---
+++ nemu/src/cpu/exec/arith/add.h
@@ -0,0 +1,22 @@
+#ifndef __ADD_H__
+#define __ADD_H__
+
+make_helper(add_i2a_b);
+make_helper(add_i2rm_b);
+make_helper(add_si2rm_b);
+make_helper(add_r2rm_b);
+make_helper(add_rm2r_b);
+/*
+make_helper(mov_a2moffs_b);
+make_helper(mov_moffs2a_b);
+*/
+make_helper(add_i2a_v);
+make_helper(add_i2rm_v);
+make_helper(add_si2rm_v);
+make_helper(add_r2rm_v);
+make_helper(add_rm2r_v);
+/*
+make_helper(mov_a2moffs_v);
+make_helper(mov_moffs2a_v);
+*/
+#endif
