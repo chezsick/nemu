ad00e679508aee360fecdd5cf93e37f75fa9f452
tracer-ics2015 <tracer@njuics.org>
2015-10-18 22:27:23 +0800

    >  compile NEMU 141220162 zhouyc Linux Debian 3.16.0-4-686-pae #1 SMP Debian 3.16.7-ckt11-1 (2015-05-24) i686 GNU/Linux  22:27:23 up  3:36, 10 users,  load average: 0.01, 0.02, 0.05 3f285396d7874b4bf67924e634e02945649eb9ac

--- nemu/src/cpu/exec/transfer/jmp-template.h
+++ nemu/src/cpu/exec/transfer/jmp-template.h
@@ -16,13 +16,13 @@ make_instr_helper(i)
 make_instr_helper(rm)
 */
 make_helper(concat(jmp_i_,SUFFIX)){
-	cpu.eip+=op_src->val;
-	//cpu.eip+=instr_fetch(eip + 1, DATA_BYTE*4);
-	//printf("%x\n",cpu.eip);
+	//cpu.eip+=op_src->val;
+	cpu.eip+=instr_fetch(eip + 1, DATA_BYTE*4);
+	printf("%x\n",cpu.eip);
 	if (ops_decoded.is_data_size_16)
                 cpu.eip&=0x0000ffff;
-	//print_asm("jmp" str(SUFFIX) " 0x%x",cpu.eip);
-	print_asm_template1();
+	print_asm("jmp" str(SUFFIX) " 0x%x",cpu.eip);
+	//print_asm_template1();
 	return 2;
 }
 make_helper(concat(jmp_rm_,SUFFIX)){
