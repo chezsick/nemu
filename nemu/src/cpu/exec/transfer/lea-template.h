ab06624c4078ba4138591b31d6ef072f6e129216
tracer-ics2015 <tracer@njuics.org>
2015-10-18 11:42:18 +0800

    >  compile NEMU 141220162 zhouyc Linux Debian 3.16.0-4-686-pae #1 SMP Debian 3.16.7-ckt11-1 (2015-05-24) i686 GNU/Linux  11:42:18 up  1:52,  9 users,  load average: 0.00, 0.01, 0.05 c6c9da13e4bf404a1a1ea4b09b4fd11d67463366

--- nemu/src/cpu/exec/transfer/lea-template.h
+++ nemu/src/cpu/exec/transfer/lea-template.h
@@ -2,17 +2,11 @@
 #define instr lea
 
 static void do_execute() {
-	//DATA_TYPE mem=instr_fetch(cpu.eip + 1, 4);
-	//printf("**%x\n",mem);
 	if (DATA_BYTE==2){
-		OPERAND_W(op_dest,op_src->simm);
+		OPERAND_W(op_dest,op_src->simm&0xffff);
 	}
 	else {
-		//printf("before lea: %x, %x\n",op_src->val,op_src2->val);
-		//op_dest->val=op_src->val+mem;
-		//DATA_TYPE mem=instr_fetch(eip+1,DATA_BYTE);
 		OPERAND_W(op_dest,op_src->simm);
-		//printf("after lea: %x, %x\n",cpu.eax,cpu.edx);
 	}
 	print_asm_template2();
 }
