adf3a606e4760c18cdcedcc6f20a87104cd61298
tracer-ics2015 <tracer@njuics.org>
2015-10-19 22:05:43 +0800

    >  compile NEMU 141220162 zhouyc Linux Debian 3.16.0-4-686-pae #1 SMP Debian 3.16.7-ckt11-1 (2015-05-24) i686 GNU/Linux  22:05:43 up  3:39,  9 users,  load average: 0.00, 0.01, 0.05 c23fc17463f78d9b4d250d653592b8304a432bf1

--- nemu/src/cpu/exec/arith/adc-template.h
+++ nemu/src/cpu/exec/arith/adc-template.h
@@ -3,7 +3,7 @@
 #define instr adc
 
 static void do_execute() {
-	DATA_TYPE result = op_dest->val + op_src->val;//+cpu.EFLAGS.CF;
+	DATA_TYPE result = op_dest->val + op_src->val+cpu.EFLAGS.CF;
 	OPERAND_W(op_dest, result);
 	DATA_TYPE p=result;
 	uint32_t i;
