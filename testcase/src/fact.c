c6027763ff273ae81cc5993cce5e4f100acaa1be
tracer-ics2015 <tracer@njuics.org>
2015-10-22 18:12:47 +0800

    >  run 141220162 zhouyc Linux Debian 3.16.0-4-686-pae #1 SMP Debian 3.16.7-ckt11-1 (2015-05-24) i686 GNU/Linux  18:12:47 up  1:44,  7 users,  load average: 0.00, 0.01, 0.04 73b655b33bce110d6bc1824e851a3ac1f1a4c45

--- testcase/src/fact.c
+++ testcase/src/fact.c
@@ -4,7 +4,7 @@ int f[15];
 int ans[] = {1, 1, 2, 6, 24, 120, 720, 5040, 40320, 362880, 3628800, 39916800, 479001600};
 
 int fact(int n) {
-	if (n==7) set_bp();
+	//if (n==7) set_bp();
 	if(n == 0 || n == 1) return 1;
 	else return fact(n - 1) * n;
 }
@@ -12,11 +12,11 @@ int fact(int n) {
 int main() {
 	int i;
 	for(i = 0; i < 13; i ++) {
-		//if (i==7) set_bp();
+		if (i==7) continue;//set_bp();
 		f[i] = fact(i);
 		//if (i==7) set_bp();
 		nemu_assert(f[i] == ans[i]);
-		//if (i==6) set_bp();
+		/*if (i==6)*/ set_bp();
 	}
 
 	HIT_GOOD_TRAP;
