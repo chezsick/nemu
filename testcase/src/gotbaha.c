ff3f2087acac50cf80e56677aa97beea97cea1a1
tracer-ics2015 <tracer@njuics.org>
2015-10-23 00:01:44 +0800

    >  run 141220162 zhouyc Linux Debian 3.16.0-4-686-pae #1 SMP Debian 3.16.7-ckt11-1 (2015-05-24) i686 GNU/Linux  00:01:44 up 36 min,  8 users,  load average: 0.00, 0.02, 0.05 da8bf7eb6f5d195a082fca9cc5f377c52ff8871

--- testcase/src/gotbaha.c
+++ testcase/src/gotbaha.c
@@ -15,6 +15,7 @@ int is_prime(int n) {
 
 int gotbaha(int n) {
 	int i;
+	if (n==8) set_bp();
 	for(i = 2; i < n; i ++) {
 		if(is_prime(i) && is_prime(n - i)) {
 			return 1;
@@ -27,7 +28,7 @@ int gotbaha(int n) {
 int main() {
 	int n;
 	for(n = 4; n <= 100; n += 2) {
-		set_bp();
+		//set_bp();
 		nemu_assert(gotbaha(n) == 1);
 	}
 
