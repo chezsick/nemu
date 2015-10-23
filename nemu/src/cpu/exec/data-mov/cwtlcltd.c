7182d28625fb3b8eaf5c92c2c7bc1e92c92c5d28
tracer-ics2015 <tracer@njuics.org>
2015-10-23 22:44:15 +0800

    >  compile NEMU 141220162 zhouyc Linux Debian 3.16.0-4-686-pae #1 SMP Debian 3.16.7-ckt11-1 (2015-05-24) i686 GNU/Linux  22:44:15 up  1:05,  9 users,  load average: 0.00, 0.01, 0.05 37b6619e2fe4aa9ad46b830828ad49d2e067fc52

--- nemu/src/cpu/exec/data-mov/cwtlcltd.c
+++ nemu/src/cpu/exec/data-mov/cwtlcltd.c
@@ -1,14 +1,15 @@
 #include "cpu/exec/helper.h"
 
+//#define MSB(n) ((DATA_TYPE)(n) >> ((DATA_BYTE << 3) -1))
 /* for instruction encoding overloading */
 #define instr leave
 make_helper(cwtlcltd){
 	if (ops_decoded.is_data_size_16){
-		reg_w(R_DX)=(reg_w(R_AX)<0)?0xffff:0;
+		reg_w(R_DX)=(reg_w(R_AX)>>15)?0xffff:0;
 		print_asm("cwtl");
 	}
 	else{
-		reg_l(R_EDX)=(reg_l(R_EAX)<0)?0xffffffff:0;
+		reg_l(R_EDX)=(reg_l(R_EAX)>>31)?0xffffffff:0;
 		print_asm("cltd");	
 	}
 	return 1;
