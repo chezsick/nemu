b20453269a06848fe6f9997d74363c8ebe9d2782
tracer-ics2015 <tracer@njuics.org>
2015-10-24 20:57:29 +0800

    >  compile NEMU 141220162 zhouyc Linux Debian 3.16.0-4-686-pae #1 SMP Debian 3.16.7-ckt11-1 (2015-05-24) i686 GNU/Linux  20:57:29 up  7:28,  8 users,  load average: 0.00, 0.01, 0.05 6327541e75bd3bd41cc85bfb27c72fa9c7306876

--- nemu/src/cpu/exec/arith/cmp-template.h
+++ nemu/src/cpu/exec/arith/cmp-template.h
@@ -5,8 +5,8 @@
 static void do_execute() {
 	DATA_TYPE_S result = op_dest->val - (DATA_TYPE_S)op_src->val;
 	//OPERAND_W(op_dest, result);
-	//printf("%d-%d=%d\n",op_dest->val,op_src->val,result);
-	DATA_TYPE_S p=result;
+	printf("%d-%d=%d\n",op_dest->val,op_src->val,result);
+	DATA_TYPE p=result;
 	uint32_t i;
 	p=p&0xff;
 	p^=p>>4;
