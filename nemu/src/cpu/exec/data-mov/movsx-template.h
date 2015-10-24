d252653cbfb822dabb66953c8e9cf35d1e6f694e
tracer-ics2015 <tracer@njuics.org>
2015-10-24 12:34:47 +0800

    >  compile NEMU 141220162 zhouyc Linux Debian 3.16.0-4-686-pae #1 SMP Debian 3.16.7-ckt11-1 (2015-05-24) i686 GNU/Linux  12:34:47 up  2:39,  8 users,  load average: 0.00, 0.01, 0.05 4528d8fed598cc7a1032ffea8db22695d255ff84

--- nemu/src/cpu/exec/data-mov/movsx-template.h
+++ nemu/src/cpu/exec/data-mov/movsx-template.h
@@ -1,15 +1,25 @@
 #include "cpu/exec/template-start.h"
 
-#define instr movsx
+#define instr movsb
 
 static void do_execute() {
 	DATA_TYPE result;
-	int shift=op_dest->type-op_src->type;
+	int shift=(DATA_BYTE-1)*8;
 	result=(op_src->val<<shift)>>shift;
 	OPERAND_W(op_dest, result);
 	print_asm_template2();
 }
 
 make_instr_helper(rm2r)
+#undef instr
+
+#define instr movsw
+static void do_execute() {
+	DATA_TYPE result=(op_src->val<<8)>>8;
+	OPERAND_W(op_dest,result);
+	print_asm_template2();
+}
+make_instr_helper(rm2r)
+#undef instr
 
 #include "cpu/exec/template-end.h"
--- nemu/src/cpu/exec/data-mov/movzx-template.h
+++ nemu/src/cpu/exec/data-mov/movzx-template.h
@@ -1,15 +1,23 @@
 #include "cpu/exec/template-start.h"
 
-#define instr movzx
+#define instr movzb
 
 static void do_execute() {
-	DATA_TYPE result=0x0|op_src->val;
+	DATA_TYPE result=0xff&op_src->val;
 	//int shift=op_dest->type-op_src->type;
 	//result=(op_src<<shift)>>shift;
 	OPERAND_W(op_dest, result);
 	print_asm_template2();
 }
-
 make_instr_helper(rm2r)
+#undef instr
 
+#define instr movzw
+static void do_execute() {
+	DATA_TYPE result= 0xffff&op_src->val;
+	OPERAND_W(op_dest,result);
+	print_asm_template2();
+}
+make_instr_helper(rm2r)
+#undef instr
 #include "cpu/exec/template-end.h"
