909eb23a4c7f2e74eb2dea0adb270006c98f3b40
tracer-ics2015 <tracer@njuics.org>
2015-10-28 22:46:07 +0800

    >  compile NEMU 141220162 zhouyc Linux Debian 3.16.0-4-686-pae #1 SMP Debian 3.16.7-ckt11-1 (2015-05-24) i686 GNU/Linux  22:46:07 up  9:58, 10 users,  load average: 0.00, 0.01, 0.05 10c290d673cfe8b23a6027c6ed90c9a676364e5b

--- nemu/src/cpu/exec/string/stos-template.h
+++ nemu/src/cpu/exec/string/stos-template.h
@@ -1,20 +1,17 @@
 #include "cpu/exec/template-start.h"
 
 //#define instr stos
-/*
-make_helper(stos_b) {
 
-	printf("eip=%x\n",cpu.eip);
+make_helper(concat(stos_, SUFFIX)) {
+	//printf("eip=%x\n",cpu.eip);
 	uint32_t destreg=cpu.edi;
-	//if (DATA_BYTE==1){
+	if (DATA_BYTE==1){
 		MEM_W(destreg,REG(R_AL));
 		if (cpu.EFLAGS.DF==0) destreg+=1;else destreg-=1;
-	print_asm("stos %%esi,%%edi\n");
-	return 1;
-}
-*/
-make_helper(concat(stos_, SUFFIX)){
-		uint32_t destreg=cpu.edi;
+	}
+	else{
+//make_helper(concat(stos_, SUFFIX)){
+		//uint32_t destreg=cpu.edi;
 		if (DATA_BYTE==2){
 			MEM_W(destreg,REG(R_AX));
 			if (cpu.EFLAGS.DF==0) destreg+=2;else destreg-=2;
@@ -23,6 +20,7 @@ make_helper(concat(stos_, SUFFIX)){
 			MEM_W(destreg,REG(R_EAX));
 			if (cpu.EFLAGS.DF==0) destreg+=4;else destreg-=4;
 		}
+	}	
 	cpu.edi=destreg;
 	//print_asm_template2();
 	print_asm("stos %%esi,%%edi\n");
