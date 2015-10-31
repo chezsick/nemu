8ab9d457b80012324d69a13114073758ee8d8f38
tracer-ics2015 <tracer@njuics.org>
2015-10-31 17:09:54 +0800

    >  run 141220162 zhouyc Linux Debian 3.16.0-4-686-pae #1 SMP Debian 3.16.7-ckt11-1 (2015-05-24) i686 GNU/Linux  17:09:54 up  4:15, 10 users,  load average: 0.09, 0.04, 0.05 d703fa8c498e48e0f230f1e3c85e9ec690d80b00

--- lib-common/FLOAT.h
+++ lib-common/FLOAT.h
@@ -6,23 +6,28 @@
 typedef int FLOAT;
 
 static inline int F2int(FLOAT a) {
-	nemu_assert(0);
-	return 0;
+	//nemu_assert(0);
+	//return 0;
+	//int result=a>>16;
+	return a>>16;
 }
 
 static inline FLOAT int2F(int a) {
-	nemu_assert(0);
-	return 0;
+	//nemu_assert(0);
+	//return 0;
+	return a<<16;
 }
 
 static inline FLOAT F_mul_int(FLOAT a, int b) {
-	nemu_assert(0);
-	return 0;
+	//nemu_assert(0);
+	//return 0;
+	return a*b;
 }
 
 static inline FLOAT F_div_int(FLOAT a, int b) {
-	nemu_assert(0);
-	return 0;
+	//nemu_assert(0);
+	//return 0;
+	return a/b;
 }
 
 FLOAT f2F(float);
