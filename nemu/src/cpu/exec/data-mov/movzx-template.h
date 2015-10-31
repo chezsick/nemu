951bc197799599af846a96804916df64d59e6059
tracer-ics2015 <tracer@njuics.org>
2015-10-31 12:16:30 +0800

    >  compile NEMU 141220162 zhouyc Linux Debian 3.16.0-4-686-pae #1 SMP Debian 3.16.7-ckt11-1 (2015-05-24) i686 GNU/Linux  12:16:30 up  2:02,  9 users,  load average: 0.01, 0.02, 0.05 5cdee8ee26b80c922ecc87772da9d6cb373def06

--- nemu/src/cpu/exec/data-mov/movzx-template.h
+++ nemu/src/cpu/exec/data-mov/movzx-template.h
@@ -3,12 +3,12 @@
 #define instr movzb
 
 static void do_execute() {
-	printf("before:%x\n",op_src->val);
+	//printf("before:%x\n",op_src->val);
 	DATA_TYPE result=0xff&op_src->val;
 	//int shift=op_dest->type-op_src->type;
 	//result=(op_src<<shift)>>shift;
 	//printf("dest:%x\n",op_dest->val);
-	printf("res=%x\n",result);
+	//printf("res=%x\n",result);
 	OPERAND_W(op_dest, result);
 	//printf("??%x\n",op_dest->val);
 	//printf("addr:%x***%c\n",op_src->addr,result);
