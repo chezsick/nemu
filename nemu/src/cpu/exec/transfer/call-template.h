d089bfa8eecfd2aaff8285b3323ec4a86a0f1fe9
tracer-ics2015 <tracer@njuics.org>
2015-10-31 22:38:41 +0800

    >  compile NEMU 141220162 zhouyc Linux Debian 3.16.0-4-686-pae #1 SMP Debian 3.16.7-ckt11-1 (2015-05-24) i686 GNU/Linux  22:38:41 up  9:44, 10 users,  load average: 0.00, 0.01, 0.05 ceed7eb2ddd13cc64bf99ac99f2e9aca9ce39547

--- nemu/src/cpu/exec/transfer/call-template.h
+++ nemu/src/cpu/exec/transfer/call-template.h
@@ -3,24 +3,38 @@
 #define instr call
 
 static void do_execute() {
-	if (ops_decoded.is_data_size_16){ //n=2;
-		cpu.esp-=2;
-		MEM_W(cpu.esp,(cpu.eip&0xff)+DATA_BYTE);
-		if (ops_decoded.opcode==0xe8){
+	if (ops_decoded.opcode==0xe8){
+		if (ops_decoded.is_data_size_16){ //n=2;
+			cpu.esp-=2;
+			MEM_W(cpu.esp,(cpu.eip&0xff)+DATA_BYTE);
+			//if (ops_decoded.opcode==0xe8){
 			cpu.eip+=op_src->val&0xffff;	
+			//}
+			/*if (ops_decoded.opcode==0xff){
+				cpu.eip=((op_src->val)&0xffff)-2;
+			}*/
 		}
-		if (ops_decoded.opcode==0xff){
-			cpu.eip=((op_src->val)&0xffff)-2;
+		else {
+			cpu.esp-=4;
+			MEM_W(cpu.esp,cpu.eip+DATA_BYTE);
+			//if (ops_decoded.opcode==0xe8){
+			cpu.eip+=op_src->val;
+			//}
+			/*if (ops_decoded.opcode==0xff){
+				cpu.eip=(op_src->val)-2;
+			}*/
 		}
 	}
-	else {
-		cpu.esp-=4;
-		MEM_W(cpu.esp,cpu.eip+DATA_BYTE);
-		if (ops_decoded.opcode==0xe8){
-			cpu.eip+=op_src->val;
+	if (ops_decoded.opcode==0xff){
+		if (ops_decoded.is_data_size_16){
+			cpu.esp-=2;
+			MEM_W(cpu.esp,(cpu.eip&0xff)+2);
+			cpu.eip=((op_src->val)&0xffff)-2;
 		}
-		if (ops_decoded.opcode==0xff){
-			cpu.eip=(op_src->val)-2;
+		else{
+			cpu.esp-=4;
+			MEM_W(cpu.esp,cpu.eip+2);
+			cpu.eip=(op_src->val)-2;	
 		}
 	}
 	print_asm_template1();
