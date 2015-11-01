6cfcce28c6d96f2ad9f745b1e45d386771fcfd49
tracer-ics2015 <tracer@njuics.org>
2015-11-02 00:38:24 +0800

    >  compile NEMU 141220162 zhouyc Linux Debian 3.16.0-4-686-pae #1 SMP Debian 3.16.7-ckt11-1 (2015-05-24) i686 GNU/Linux  00:38:24 up  8:19,  8 users,  load average: 0.10, 0.04, 0.05 46026368961f9efdf1a5eed45728bb3b030be57

--- nemu/src/monitor/debug/expr.c
+++ nemu/src/monitor/debug/expr.c
@@ -342,7 +342,7 @@ uint32_t eval(int p, int q)
 
 uint32_t expr(char *e, bool *success) {
 	match_sym(e);
-	printf("%s",e);
+	printf("%s:%d\n",e,strlen(e));
 	if(!make_token(e)) {
 		*success = false;
 		return 0;
