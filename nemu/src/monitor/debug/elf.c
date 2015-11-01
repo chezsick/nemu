de2e6e9ed75eecc1113211617c8c88af94d82564
tracer-ics2015 <tracer@njuics.org>
2015-11-02 00:25:48 +0800

    >  compile NEMU 141220162 zhouyc Linux Debian 3.16.0-4-686-pae #1 SMP Debian 3.16.7-ckt11-1 (2015-05-24) i686 GNU/Linux  00:25:48 up  8:07,  8 users,  load average: 0.00, 0.01, 0.05 a682494a179bf50713efa675192cd15b5fb04164

--- nemu/src/monitor/debug/elf.c
+++ nemu/src/monitor/debug/elf.c
@@ -103,6 +103,7 @@ void match_sym(char* strexp){
 				if(pre!=NULL){
 					sprintf(new_expr,"%s0x%08x\n",pre,symtab[i].st_value);	
 				}
+				printf("%s\n",new_expr);
 				strcat(new_expr,loc+strlen(name));
 				strcmp(strexp,new_expr);
 				printf("%s\n",strexp);
