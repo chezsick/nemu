a0313276bb97bed4d27068b500b6616c6d028e92
tracer-ics2015 <tracer@njuics.org>
2015-11-01 22:59:52 +0800

    >  compile NEMU 141220162 zhouyc Linux Debian 3.16.0-4-686-pae #1 SMP Debian 3.16.7-ckt11-1 (2015-05-24) i686 GNU/Linux  22:59:52 up  6:41,  8 users,  load average: 0.06, 0.03, 0.05 bfb9e980fd8b393ebf45a833ce6a798ac2d3d26

--- nemu/src/monitor/debug/elf.c
+++ nemu/src/monitor/debug/elf.c
@@ -82,7 +82,7 @@ void load_elf_tables(int argc, char *argv[]) {
  }
 void match_sym(char* strexp){
 	int i;
-	printf("ok\n");
+	printf("nr_symtab:%x\n",nr_symtab_entry);
 	for (i=0;i<nr_symtab_entry;i++){
 		if (symtab[i].st_info==STT_OBJECT){
 			printf("name:%x\tval:%x\n",symtab[i].st_name,symtab[i].st_value);
