e461c68c2e38989bff33043a33b5a8f6e82faa61
tracer-ics2015 <tracer@njuics.org>
2015-11-02 19:53:12 +0800

    >  compile NEMU 141220162 zhouyc Linux Debian 3.16.0-4-686-pae #1 SMP Debian 3.16.7-ckt11-1 (2015-05-24) i686 GNU/Linux  19:53:12 up 12:59,  9 users,  load average: 0.00, 0.01, 0.05 c08b9fc32df666899bed416fc6ef6d1f3ed06e5

--- nemu/src/cpu/exec/transfer/ret.c
+++ nemu/src/cpu/exec/transfer/ret.c
@@ -20,13 +20,13 @@ make_helper(ret){
 	uint32_t imm16=instr_fetch(cpu.eip+1,2);
 	if (ops_decoded.is_data_size_16){
 		//cpu.eip&=0xffffff00;
-		cpu.eip=swaddr_read(cpu.esp,2)+1;
+		cpu.eip=swaddr_read(cpu.esp,2);
 		cpu.esp+=2;
 		cpu.eip&=0x0000ffff;
 	}
 	else{
 		//printf("%x\n",cpu.eip);
-		cpu.eip=swaddr_read(reg_l(R_ESP),4)+1;
+		cpu.eip=swaddr_read(reg_l(R_ESP),4);
 		cpu.esp+=4;
 		//printf("%x\n",cpu.eip);
 	}
@@ -38,11 +38,11 @@ make_helper(ret){
 		print_asm("ret $0x%x", imm16);
 		//return 3;
 	}
-	else{
+ 	else{
 		print_asm("ret");
 	}
 	//printf("%d\n",cpu.eip);
-	return 0;
+	return 1;
 }
 /*
 make_helper_v(sub_i2rm)
