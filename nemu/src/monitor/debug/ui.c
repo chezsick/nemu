8d3ff9a4133fc2a19fab2ff83ff2a7c93c185b3c
tracer-ics2015 <tracer@njuics.org>
2015-11-02 19:54:05 +0800

    >  compile NEMU 141220162 zhouyc Linux Debian 3.16.0-4-686-pae #1 SMP Debian 3.16.7-ckt11-1 (2015-05-24) i686 GNU/Linux  19:54:05 up 13:00,  9 users,  load average: 0.00, 0.01, 0.05 1b5797c4388bb23cfabb5dd8bb922a62ceb45cf3

--- nemu/src/monitor/debug/ui.c
+++ nemu/src/monitor/debug/ui.c
@@ -187,6 +187,7 @@ static int cmd_bt(char *args)
 	while (prev_ebp!=0){
 		prev_ebp=swaddr_read(prev_ebp,4);
 		ret_addr=swaddr_read(prev_ebp+4,4);
+		if (prev_ebp==0) break;
 		printf("#%d  0x%x  in ",count++,ret_addr+1);
 		char* FunName=fun_name(ret_addr);
 		if (FunName!=NULL) printf("%s(",FunName);
