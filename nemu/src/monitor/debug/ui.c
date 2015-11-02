905d603558cd79696c88d53d705ae25def83991c
tracer-ics2015 <tracer@njuics.org>
2015-11-02 19:10:04 +0800

    >  compile NEMU 141220162 zhouyc Linux Debian 3.16.0-4-686-pae #1 SMP Debian 3.16.7-ckt11-1 (2015-05-24) i686 GNU/Linux  19:10:04 up 12:16,  9 users,  load average: 0.00, 0.01, 0.05 ba7274dbcfa9a221fa21d7dd252ca2a42a0d5c9b

--- nemu/src/monitor/debug/ui.c
+++ nemu/src/monitor/debug/ui.c
@@ -190,7 +190,7 @@ static int cmd_bt(char *args)
 		printf("#%d  0x%x  in ",count++,ret_addr);
 		char* FunName=fun_name(cpu.eip);
 		printf("%s(",FunName);
-		swaddr_t var_addr=prev_ebp+8;
+		unsigned int var_addr=prev_ebp+8;
 		int n=4;
 		while (n>0) {
 		       printf("%x\t",swaddr_read(var_addr,4));
