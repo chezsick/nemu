c5079048e88c639effcdfd6a3b8e03964d02c3ef
tracer-ics2015 <tracer@njuics.org>
2015-11-04 17:09:19 +0800

    >  compile kernel 141220162 zhouyc Linux Debian 3.16.0-4-686-pae #1 SMP Debian 3.16.7-ckt11-1 (2015-05-24) i686 GNU/Linux  17:09:19 up 4 min,  4 users,  load average: 0.02, 0.05, 0.03 fb3144577e4d67459c2c2027ad08a723b5b5de2

--- kernel/src/elf/elf.c
+++ kernel/src/elf/elf.c
@@ -31,25 +31,25 @@ uint32_t loader() {
 	elf = (void*)buf;
 
 	/* TODO: fix the magic number with the correct one */
-	const uint32_t elf_magic = 0x7f454c46;
+	const uint32_t elf_magic = 0x464c457f;
 	uint32_t *p_magic = (void *)buf;
 	nemu_assert(*p_magic == elf_magic);
-
+	
 	/* Load each program segment */
 	panic("please implement me");
-	for(; true; ) {
+	for(; true; ) { 
 		/* Scan the program header table, load each segment into memory */
 		if(ph->p_type == PT_LOAD) {
 
-			/* TODO: read the content of the segment from the ELF file 
+	 		/* TODO: read the content of the segment from the ELF file 
 			 * to the memory region [VirtAddr, VirtAddr + FileSiz)
 			 */
 			ramdisk_read(buf, ph->p_vaddr,ph->p_memsz);
 			 
-			/* TODO: zero the memory region 
+	 		/* TODO: zero the memory region 
 			 * [VirtAddr + FileSiz, VirtAddr + MemSiz)
 			 */
-			memset(ph->p_vaddr+ph->p_filesz,0,ph->p_memsz-ph->p_filesz);
+			//memset(ph->p_vaddr+ph->p_filesz,0,ph->p_memsz-ph->p_filesz);
 
 #ifdef IA32_PAGE
 			/* Record the program break for future use. */
@@ -57,7 +57,7 @@ uint32_t loader() {
 			uint32_t new_brk = ph->p_vaddr + ph->p_memsz - 1;
 			if(brk < new_brk) { brk = new_brk; }
 #endif
-		}
+	 	}
 	}
 
 	volatile uint32_t entry = elf->e_entry;
