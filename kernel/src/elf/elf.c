ed03cbd4195ca9b4305338ed15fc1b39e00ab949
tracer-ics2015 <tracer@njuics.org>
2015-11-04 21:07:31 +0800

    >  compile kernel 141220162 zhouyc Linux Debian 3.16.0-4-686-pae #1 SMP Debian 3.16.7-ckt11-1 (2015-05-24) i686 GNU/Linux  21:07:31 up  4:02,  8 users,  load average: 0.00, 0.08, 0.09 9fb328beacb8d395a986e57e4cd991d1f09ec79

--- kernel/src/elf/elf.c
+++ kernel/src/elf/elf.c
@@ -60,6 +60,7 @@ uint32_t loader() {
 			 */
 			 
 			memset((void*)ph->p_vaddr+ph->p_filesz, 0, ph->p_memsz-ph->p_filesz);
+			set_bp();
 
 #ifdef IA32_PAGE
 			/* Record the program break for future use. */
@@ -67,7 +68,6 @@ uint32_t loader() {
 			extern uint32_t brk;
 			uint32_t new_brk = ph->p_vaddr + ph->p_memsz - 1;
 			if(brk < new_brk) { brk = new_brk; }
-			set_bp();
 #endif
 	 	}
 	}
