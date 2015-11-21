22bbe9ce17680c3d11e8dd413802719f79ee6b42
tracer-ics2015 <tracer@njuics.org>
2015-11-21 21:53:34 +0800

    >  compile NEMU 141220162 zhouyc Linux Debian 3.16.0-4-686-pae #1 SMP Debian 3.16.7-ckt11-1 (2015-05-24) i686 GNU/Linux  21:53:34 up  7:47,  7 users,  load average: 0.00, 0.01, 0.05 85e038543396e6b7d3ffe60a866c57d92cf9c9aa

--- nemu/src/memory/L2cache.c
+++ nemu/src/memory/L2cache.c
@@ -132,7 +132,8 @@ void L2cache_write(hwaddr_t addr, size_t len, uint32_t data){
 	bool is_hit;
 	Assert(addr < HW_MEM_SIZE, "physical address %x is outside of the physical memory!(in L2cache)", addr);
 	is_hit=L2hit(addr, &hit_index);
-	if (!is_hit==true){			//miss
+	if (!is_hit==true){
+		dram_write(addr, len, data); 			//miss
 		L2cache_read(addr, len);
 	}
 	*(uint32_t *)(temp + offset)=data;
