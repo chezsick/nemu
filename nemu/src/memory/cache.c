f8faf069e52b8346ec3c8d01861ff9127566c5da
tracer-ics2015 <tracer@njuics.org>
2015-11-21 15:40:33 +0800

    >  compile NEMU 141220162 zhouyc Linux Debian 3.16.0-4-686-pae #1 SMP Debian 3.16.7-ckt11-1 (2015-05-24) i686 GNU/Linux  15:40:33 up  1:34,  7 users,  load average: 0.01, 0.02, 0.05 2b3a7fe899228edefd57384e97c2b354e6ab5983

--- nemu/src/memory/cache.c
+++ nemu/src/memory/cache.c
@@ -50,11 +50,11 @@ bool hit(hwaddr_t addr, uint32_t* hit_index){ //if hit return hit address, else 
 		if ((cache[index+i].valid) && (cache[index+i].tag==addr_tag)){
 			is_hit=true;
 			*hit_index=index+i;
-			Log("hit! in %x", addr);
+			//Log("hit! in %x", addr);
 			break;
 	 	}
 	}
-	if (!is_hit) Log("%x: miss!", addr);
+	//if (!is_hit) Log("%x: miss!", addr);
 	return is_hit;
 }
 
@@ -91,12 +91,14 @@ uint32_t cache_read(hwaddr_t addr, size_t len){
 	
 	Assert(addr < HW_MEM_SIZE, "physical address %x is outside of the physical memory!(in cache)", addr);
 	memcpy(temp, cache[hit_index].block, BLOCK_SIZE);
+	/*
 	int i;
 	printf("temp:");
 	for (i=0;i<2*BLOCK_SIZE;i++){
 		printf("%0x",temp[i]&0xff);
 	}
 	printf("\n");
+	*/
  	if (offset + len > BLOCK_SIZE) {
 		/* data cross the slot boundary */
 		Log("cross the boundary!");
