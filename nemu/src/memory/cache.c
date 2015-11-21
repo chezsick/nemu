5f02790305e34653b651aad3c1329db06fce1361
tracer-ics2015 <tracer@njuics.org>
2015-11-21 19:39:26 +0800

    >  compile NEMU 141220162 zhouyc Linux Debian 3.16.0-4-686-pae #1 SMP Debian 3.16.7-ckt11-1 (2015-05-24) i686 GNU/Linux  19:39:26 up  5:33,  7 users,  load average: 0.00, 0.01, 0.05 f90ed5b447befa8256c652ef1e7bb0d3b2a58de2

--- nemu/src/memory/cache.c
+++ nemu/src/memory/cache.c
@@ -105,6 +105,7 @@ uint32_t cache_read(hwaddr_t addr, size_t len){
 		/* data cross the slot boundary */
 		Log("cross the boundary!");
 		*(temp+BLOCK_SIZE)=cache_read(addr -offset + BLOCK_SIZE, offset + len - BLOCK_SIZE);
+		printf("cross:%x\n", unalign_rw(temp + offset, 4));
 	}
 	return unalign_rw(temp + offset, 4);
 	
