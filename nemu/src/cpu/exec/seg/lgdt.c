81b2c8c4de25767f1eeadcbd9f7c7fd2a92a30f1
tracer-ics2015 <tracer@njuics.org>
2015-12-09 16:02:59 +0800

    >  compile NEMU 141220162 zhouyc Linux Debian 3.16.0-4-686-pae #1 SMP Debian 3.16.7-ckt11-1 (2015-05-24) i686 GNU/Linux  16:02:59 up  2:00,  7 users,  load average: 0.00, 0.01, 0.05 1c5855df44d7c23df44aaa9b3defa8cea4ae1abd

--- nemu/src/cpu/exec/seg/lgdt.c
+++ nemu/src/cpu/exec/seg/lgdt.c
@@ -2,8 +2,7 @@
 #include "cpu/decode/modrm.h"
 
 make_helper(lgdt_m) {
-         printf("lgdt.c line 5\n");
-	 swaddr_t addr=instr_fetch(eip+2, 4);
+         swaddr_t addr=instr_fetch(eip+1, 4);
          cpu.gdtr.limit = swaddr_read(addr, 2, R_SS);
          cpu.gdtr.base = swaddr_read(addr+2, 4, R_SS);
          print_asm("lgdt");
