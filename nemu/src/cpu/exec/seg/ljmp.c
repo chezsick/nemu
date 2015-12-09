e230c87eeea0a0ca4d4f638b1867a9931ed2d9f6
tracer-ics2015 <tracer@njuics.org>
2015-12-09 20:23:54 +0800

    >  compile NEMU 141220162 zhouyc Linux Debian 3.16.0-4-686-pae #1 SMP Debian 3.16.7-ckt11-1 (2015-05-24) i686 GNU/Linux  20:23:54 up  4:46,  8 users,  load average: 0.00, 0.01, 0.05 920f373b5760e4e6c5310f33dbb8c2af2e6e7b1

--- nemu/src/cpu/exec/seg/ljmp.c
+++ nemu/src/cpu/exec/seg/ljmp.c
@@ -2,8 +2,8 @@
 
 make_helper(jmp_ptr){
 	cpu.eip = instr_fetch(eip+1, 4);
-	cpu.eip-=7;
+	//cpu.eip-=7;
 	cpu.sreg[R_CS].Selector.val = instr_fetch(eip+5, 2);
 	print_asm("ljmp $%x, $%x", cpu.sreg[R_CS].Selector.val, cpu.eip);
-	return 7;
+	return 0;
 }
