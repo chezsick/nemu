64e2881443e118c93a7a0b3f073d1bd4f2d579d3
tracer-ics2015 <tracer@njuics.org>
2015-12-09 14:51:09 +0800

    >  compile NEMU 141220162 zhouyc Linux Debian 3.16.0-4-686-pae #1 SMP Debian 3.16.7-ckt11-1 (2015-05-24) i686 GNU/Linux  14:51:09 up 48 min,  7 users,  load average: 0.08, 0.03, 0.05 6b404dd9d54739b10b2d1e62cf270ff2a61b53b

--- nemu/src/cpu/exec/exec.c
+++ nemu/src/cpu/exec/exec.c
@@ -230,7 +230,6 @@ helper_fun _2byte_opcode_table [256] = {
 make_helper(exec) {
 	ops_decoded.opcode = instr_fetch(eip, 1);
 	printf("exec line 232\n");
-	assert(0);
 	return opcode_table[ ops_decoded.opcode ](eip);
 }
 
