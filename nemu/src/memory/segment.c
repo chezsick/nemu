ae571f3e7c6f7bc190f45aab7a794c1cdb9a1000
tracer-ics2015 <tracer@njuics.org>
2015-12-09 19:16:11 +0800

    >  compile NEMU 141220162 zhouyc Linux Debian 3.16.0-4-686-pae #1 SMP Debian 3.16.7-ckt11-1 (2015-05-24) i686 GNU/Linux  19:16:11 up  3:38,  9 users,  load average: 0.00, 0.01, 0.05 73fc32f7a8c0ed3f6530c678c34016ca513f566

--- nemu/src/memory/segment.c
+++ nemu/src/memory/segment.c
@@ -2,18 +2,18 @@
 #include "../../../lib-common/x86-inc/mmu.h"
 
 lnaddr_t seg_translate(swaddr_t addr, size_t len, uint8_t sr_no){
-	/*if (cpu.cr0.protect_enable==0) return addr;
-	if (cpu.sreg[sreg].valid) return cpu.sreg[sreg].base;
+	if (cpu.cr0.protect_enable==0) return addr;
+	if (cpu.sreg[sr_no].valid) return cpu.sreg[sr_no].base;
 	SegDesc sd;
 	int i;
 	for (i=0; i< sizeof(SegDesc); i++){
-		*((uint8_t *)(&sd)+i)= lnaddr_read(cpu.gdtr.base+cpu.sreg[sreg].Selector.index*sizeof(SegDesc)+i, 1);
+		*((uint8_t *)(&sd)+i)= lnaddr_read(cpu.gdtr.base+cpu.sreg[sr_no].Selector.index*sizeof(SegDesc)+i, 1);
 	}
-	cpu.sreg[sreg].base= (sd.base_31_24<<24)|(sd.base_23_16<<16)|sd.base_15_0;
-	cpu.sreg[sreg].limit=(sd.limit_19_16<<16)|sd.limit_15_0;
-	cpu.sreg[sreg].valid=true;
-	return cpu.sreg[sreg].base+addr;
-	*/
+	cpu.sreg[sr_no].base= (sd.base_31_24<<24)|(sd.base_23_16<<16)|sd.base_15_0;
+	cpu.sreg[sr_no].limit=(sd.limit_19_16<<16)|sd.limit_15_0;
+	cpu.sreg[sr_no].valid=true;
+	return cpu.sreg[sr_no].base+addr;
+	/*
 	uint16_t select = cpu.sreg[sr_no].Selector.index;
 	uint32_t buf[2];
 	lnaddr_t temp = cpu.gdtr.base + select*8;
@@ -22,9 +22,10 @@ lnaddr_t seg_translate(swaddr_t addr, size_t len, uint8_t sr_no){
 	SegDesc *des =(SegDesc *)buf;
 	lnaddr_t ln_addr;
 	uint32_t base = 0;
-	base = (des->base_31_24<<24) + (des->base_23_16 << 16) + des->base_15_0;
+	base = (des->base_31_24<<24) | (des->base_23_16 << 16) | des->base_15_0;
 	ln_addr=addr + base;
-	printf("of %x + base %x\n", addr, base);
+	//printf("of %x + base %x\n", addr, base);
 	return ln_addr;
+	*/
 
 }
