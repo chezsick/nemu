c589fa5ef1fadb90469bdef74c7fce23615fcac8
tracer-ics2015 <tracer@njuics.org>
2015-12-10 21:04:51 +0800

    >  compile NEMU 141220162 zhouyc Linux Debian 3.16.0-4-686-pae #1 SMP Debian 3.16.7-ckt11-1 (2015-05-24) i686 GNU/Linux  21:04:51 up  2:20,  7 users,  load average: 0.00, 0.02, 0.05 d6049bd192dc21db7ce6e18731fab173debf307

--- nemu/src/cpu/exec/seg/mov-seg.c
+++ nemu/src/cpu/exec/seg/mov-seg.c
@@ -12,12 +12,17 @@ make_helper(mov_cr2r) {
         print_asm("mov\t %%cr%d, %%%s", m.reg, regsl[m.R_M]);
         return 2;
 }
+void init_TLB();
 make_helper(mov_r2cr) {
 	ModR_M m;
 	m.val= instr_fetch(eip+1, 1);
 	switch(m.reg){
 		case 0:cpu.cr0.val=reg_l(m.R_M); break;
-		case 3:cpu.cr3.val=reg_l(m.R_M); break;
+		case 3:{
+			cpu.cr3.val=reg_l(m.R_M); 
+			init_TLB();
+			break;
+		}
 	}
 	print_asm("mov\t %%%s, %%cr%d", regsl[m.R_M], m.reg);
 	return 2;
