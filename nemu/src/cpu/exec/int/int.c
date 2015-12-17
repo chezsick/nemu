f3fd12f2db640d2b8038da894b61c2d9a75caede
tracer-ics2015 <tracer@njuics.org>
2015-12-17 20:25:20 +0800

    >  compile NEMU 141220162 zhouyc Linux Debian 3.16.0-4-686-pae #1 SMP Debian 3.16.7-ckt11-1 (2015-05-24) i686 GNU/Linux  20:25:20 up 44 min,  6 users,  load average: 0.08, 0.03, 0.05 294b3bfc7ad138441eb9d5f44c213321d20531fb

---
+++ nemu/src/cpu/exec/int/int.c
@@ -0,0 +1,30 @@
+#include "cpu/exec/helper.h"
+#include "../../lib-common/x86-inc/mmu.h"
+#include <setjmp.h>
+extern jmp_buf jbuf;
+#define push(x){\
+	cpu.esp-=4;\
+	swaddr_write(cpu.esp, 4, x, R_SS);\
+}
+
+
+void raise_intr(uint8_t NO) {
+		/* TODO: Trigger an interrupt/exception with ``NO''.
+		 * That is, use ``NO'' to index the IDT.
+		*/	 	
+		push(cpu.EFLAGS.eflags);
+		push(cpu.CS.Selector.val);
+		push(cpu.eip);
+		GateDesc gd;
+		int i;
+		for (i = 0; i < sizeof(GateDesc); i++){
+			*((uint8_t*)(&gd)+i) = lnaddr_read(cpu.idtr.base+NO*sizeof(GateDesc)+i, 1);
+		}
+		cpu.CS.Selector.val = gd.segment;
+		cpu.CS.valid = false;
+		swaddr_t addr = (gd.offset_31_16<<16) | gd.offset_15_0;
+		cpu.eip = addr;
+		/* Jump back to cpu_exec() */
+
+		longjmp(jbuf, 1);
+}
