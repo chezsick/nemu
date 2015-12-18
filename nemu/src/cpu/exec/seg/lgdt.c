2e532affafff040c7d38569e0b7ea604a552851f
tracer-ics2015 <tracer@njuics.org>
2015-12-18 21:17:49 +0800

    >  compile NEMU 141220162 zhouyc Linux Debian 3.16.0-4-686-pae #1 SMP Debian 3.16.7-ckt11-1 (2015-05-24) i686 GNU/Linux  21:17:49 up 51 min,  3 users,  load average: 0.00, 0.01, 0.05 bdfb72382b21d8d04f275d2447771f6ce8c9d020

--- nemu/src/cpu/exec/seg/lgdt.c
+++ nemu/src/cpu/exec/seg/lgdt.c
@@ -2,16 +2,25 @@
 #include "cpu/decode/modrm.h"
 #include <assert.h>
 make_helper(lgdt_m) {
-	//assert(0);
-         swaddr_t addr=instr_fetch(eip+2, 4);
-         cpu.gdtr.limit = swaddr_read(addr, 2, R_SS);
-         cpu.gdtr.base = swaddr_read(addr+2, 4, R_SS);
-         print_asm("lgdt");
-         return 6;
+	if (instr_fetch(eip+1, 1) == 0x15){
+		swaddr_t addr = instr_fetch(eip+2, 4);
+		cpu.gdtr.limit = swaddr_read(addr, 2, R_SS);
+        	cpu.gdtr.base = swaddr_read(addr+2, 4, R_SS);
+       		print_asm("lgdt");
+        	return 6;
+	}
+	else {
+		cpu.gdtr.limit = swaddr_read(cpu.eax, 2, R_SS);
+		cpu.gdtr.base = swaddr_read(cpu.eax, 2, R_SS);
+		print_asm("lgdt (%%eax)");
+		return 2;
+	}
+	return 0;
 }
 
 make_helper(lidt_m){
-	swaddr_t addr=instr_fetch(eip+2, 4);
+	swaddr_t addr = instr_fetch(eip+2, 4);
+	//int len=read_ModR_M(eip+1, &addr, &addr);
 	cpu.idtr.limit = swaddr_read(addr, 2, R_SS);
 	cpu.idtr.base = swaddr_read(addr+2, 4, R_SS);
 	print_asm("lidt");
