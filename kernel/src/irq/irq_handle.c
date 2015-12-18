c86b5f630131b60231308c71258539eed8749712
tracer-ics2015 <tracer@njuics.org>
2015-12-18 22:50:17 +0800

    >  compile kernel 141220162 zhouyc Linux Debian 3.16.0-4-686-pae #1 SMP Debian 3.16.7-ckt11-1 (2015-05-24) i686 GNU/Linux  22:50:17 up  2:24,  4 users,  load average: 0.00, 0.04, 0.05 ab0f31f5103e4632c631064285f71314fa5d33fb

--- kernel/src/irq/irq_handle.c
+++ kernel/src/irq/irq_handle.c
@@ -34,10 +34,10 @@ void irq_handle(TrapFrame *tf) {
 	 * following line after you are done.
 	 */
 	//set_bp();
-	//panic("Have you re-organized the ``TrapFrame'' structure?");
+	panic("Have you re-organized the ``TrapFrame'' structure?");
 
 	int irq = tf->irq;
-
+	set_bp();
 	if (irq < 0) {
 		panic("Unhandled exception!");
 	} else if (irq == 0x80) {
