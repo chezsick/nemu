9717355a7a3d07312920fe9277b4081664232b37
tracer-ics2015 <tracer@njuics.org>
2015-12-22 22:06:37 +0800

    >  compile NEMU 141220162 zhouyc Linux Debian 3.16.0-4-686-pae #1 SMP Debian 3.16.7-ckt11-1 (2015-05-24) i686 GNU/Linux  22:06:37 up  1:33,  6 users,  load average: 0.00, 0.01, 0.05 4d9fc4bab16a0ec032b364a4f29a1fb7b74a81ef

--- nemu/src/cpu/exec/special/special.c
+++ nemu/src/cpu/exec/special/special.c
@@ -31,7 +31,7 @@ make_helper(nemu_trap) {
 		case 2:
 			//int i;
 			for (i=0; i<cpu.edx; i++){
-				printf("%d", swaddr_read(cpu.ecx+i, 1, R_DS));
+				printf("%c", swaddr_read(cpu.ecx+i, 1, R_DS));
 				printf("\n");
 			}
 		   	break;
