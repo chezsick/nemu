67b0029397ec2004267b3eca9717f22cc41ecef3
tracer-ics2015 <tracer@njuics.org>
2015-12-24 18:57:48 +0800

    >  compile kernel 141220162 zhouyc Linux Debian 3.16.0-4-686-pae #1 SMP Debian 3.16.7-ckt11-1 (2015-05-24) i686 GNU/Linux  18:57:48 up 9 min,  4 users,  load average: 0.00, 0.03, 0.04 62ba162aef5ff3179abb18568b476fb9eea34b43

--- kernel/src/syscall/do_syscall.c
+++ kernel/src/syscall/do_syscall.c
@@ -13,9 +13,16 @@ static void sys_brk(TrapFrame *tf) {
 	tf->eax = 0;
 }
 
+int sys_write(int fd, int buf, int len) {
+	if ((fd==1)||(fd==2)){
+		asm volatile (".byte 0xd6" : : "a"(2), "c"(buf), "d"(len));
+	}
+	return len;
+}
+
 void do_syscall(TrapFrame *tf) {
 	switch(tf->eax) {
-		/* The ``add_irq_handle'' system call is artificial. We use it to 
+ 		/* The ``add_irq_handle'' system call is artificial. We use it to 
 		 * let user program register its interrupt handlers. But this is 
 		 * very dangerous in a real operating system. Therefore such a 
 		 * system call never exists in GNU/Linux.
@@ -30,7 +37,8 @@ void do_syscall(TrapFrame *tf) {
 
 		/* TODO: Add more system calls. */
 		case SYS_write:
-			tf->eax = fs_write(tf->ebx,(void*)tf->ecx, tf->edx);
+			//tf->eax = fs_write(tf->ebx,(void*)tf->ecx, tf->edx);
+			tf->eax = sys_write(tf->ebx, tf->ecx, tf->edx);
 			break;
 		default: panic("Unhandled system call: id = %d", tf->eax);
 	}
