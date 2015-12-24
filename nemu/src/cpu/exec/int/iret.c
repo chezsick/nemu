1f60021ecb5ad987bb193318d145d0f63018d929
tracer-ics2015 <tracer@njuics.org>
2015-12-24 20:04:43 +0800

    >  compile NEMU 141220162 zhouyc Linux Debian 3.16.0-4-686-pae #1 SMP Debian 3.16.7-ckt11-1 (2015-05-24) i686 GNU/Linux  20:04:43 up  1:16,  7 users,  load average: 0.02, 0.02, 0.05 d0335949b366ce1abdd08f6c99290c8c577afe94

--- nemu/src/cpu/exec/int/iret.c
+++ nemu/src/cpu/exec/int/iret.c
@@ -2,17 +2,22 @@
 #define DATA_BYTE 4
 #include "cpu/exec/template-start.h"
 
-uint32_t pop(){
-	uint32_t result;
-	result=MEM_R(cpu.esp,R_SS);
-	cpu.esp+=DATA_BYTE;
-	return result;
-}
+#define pop() swaddr_read(cpu.esp, 4, R_SS); cpu.esp+=4;
+
 make_helper(iret) {
 	cpu.eip = pop();
 	cpu.CS.Selector.val = pop();
-	cpu.CS.valid = false;
+	//cpu.CS.valid = false;
 	cpu.EFLAGS.eflags = pop();
+
+	/*
+	uint32_t gdt_off = cpu.gdtr.base + (cpu.sreg[R_CS].index*8);
+	uint32_t base = lnaddr_read(gdt_off+2, 2);
+	base = base + (lnaddr_read(gdt_off+4, 1)<<16) + (lnaddr_read(gdt_off+7)<<24);
+	cpu.sreg[R_CS].base = base;//load base
+	uint32_t limit = lnaddr_read(gdt_off, 2) + ((lnaddr_read(gdt_off+6, 1)&0xf)<<16);
+	cpu.sreg[R_SS].limit= limit<< ((lnaddr_read(gdt_off+6, 1)>>7) *0xc);	//load limit
+	*/
 	print_asm("iret");
 	return 0;
 }
