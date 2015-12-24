adbeec6853aa742d061ab260241449f4befbd3e2
tracer-ics2015 <tracer@njuics.org>
2015-12-24 21:22:03 +0800

    >  run 141220162 zhouyc Linux Debian 3.16.0-4-686-pae #1 SMP Debian 3.16.7-ckt11-1 (2015-05-24) i686 GNU/Linux  21:22:03 up  2:33,  8 users,  load average: 0.11, 0.07, 0.05 2fb8322e46d86a5c884c952161be03ec1b438554

--- testcase/src/hello.c
+++ testcase/src/hello.c
@@ -5,11 +5,15 @@
 
 #include "trap.h"
 
+int times = 0;
+
 int __attribute__((__noinline__))
 syscall(int id, ...) {
 	int ret;
 	int *args = &id;
-	nemu_assert(*(char*)args[2]=='\0');
+	if (times == 0) nemu_assert(*(char*)args[2]=='H');
+	else if (times == 1) nemu_assert(*(char*)args[2]=='1');
+
 	asm volatile("int $0x80": "=a"(ret) : "a"(args[0]), "b"(args[1]), "c"(args[2]), "d"(args[3]));
 	return ret;
 }
