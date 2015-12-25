fe1d80c3e14715dbd2b9f820069155a306b50566
tracer-ics2015 <tracer@njuics.org>
2015-12-25 15:51:02 +0800

    >  compile kernel 141220162 zhouyc Linux Debian 3.16.0-4-686-pae #1 SMP Debian 3.16.7-ckt11-1 (2015-05-24) i686 GNU/Linux  15:51:02 up 12 min,  5 users,  load average: 0.03, 0.04, 0.05 4da2c8a5b25236eee674ff7d2ee7cf752b55bda

--- kernel/src/syscall/do_syscall.c
+++ kernel/src/syscall/do_syscall.c
@@ -15,7 +15,6 @@ static void sys_brk(TrapFrame *tf) {
 
 int sys_write(int fd, void *buf, int len) {
 	if ((fd==1)||(fd==2)){
-	assert(0);
 		asm volatile (".byte 0xd6" : : "a"(2), "c"(buf), "d"(len));
 	}
 	return len;
@@ -39,6 +38,7 @@ void do_syscall(TrapFrame *tf) {
 		/* TODO: Add more system calls. */
 		case SYS_write:
 			//tf->eax = fs_write(tf->ebx,(void*)tf->ecx, tf->edx);
+			Log("tf->ebx : %x", tf->ebx);
 			tf->eax = sys_write(tf->ebx, (void*)tf->ecx, tf->edx);
 			break;
 		default: panic("Unhandled system call: id = %d", tf->eax);
