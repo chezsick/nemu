1063e3df3cf3ed54c34167b7896bd230287fde95
tracer-ics2015 <tracer@njuics.org>
2016-01-17 16:25:48 +0800

    >  compile NEMU 141220162 zhouyc Linux Debian 3.16.0-4-686-pae #1 SMP Debian 3.16.7-ckt11-1 (2015-05-24) i686 GNU/Linux  16:25:48 up  3:53,  6 users,  load average: 0.06, 0.03, 0.05 41af68976e89561baff86b5cd499f3b8d4bfa5c3

--- nemu/src/cpu/exec/data-mov/movzx-template.h
+++ nemu/src/cpu/exec/data-mov/movzx-template.h
@@ -6,7 +6,7 @@ static void do_execute() {
 	//printf("before:%x\n",op_src->val);
 	DATA_TYPE result=0xff&op_src->val;
 	OPERAND_W(op_dest, result);
-	Log("mov %x to %x", op_src->val, result);
+	Log("mov %x to %c", op_src->val, result);
 	print_asm_template2();
 }
 make_instr_helper(rm2r)
