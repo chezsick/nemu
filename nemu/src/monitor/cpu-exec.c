7e49594ddb356fbee50a4b57afee6c5101edada2
tracer-ics2015 <tracer@njuics.org>
2016-01-17 20:54:34 +0800

    >  compile NEMU 141220162 zhouyc Linux Debian 3.16.0-4-686-pae #1 SMP Debian 3.16.7-ckt11-1 (2015-05-24) i686 GNU/Linux  20:54:34 up  7:37,  6 users,  load average: 0.00, 0.01, 0.05 2010a1ab56a08efe7f8f7ade633d050a8c5ea2a

--- nemu/src/monitor/cpu-exec.c
+++ nemu/src/monitor/cpu-exec.c
@@ -84,7 +84,7 @@ void cpu_exec(volatile uint32_t n) {
 
 	if(nemu_state == RUNNING) { nemu_state = STOP; }
 
-	if(cpu.INTR & cpu.EFLAGS.IF) {
+	if(cpu.INTR && cpu.EFLAGS.IF) {
 		uint32_t intr_no = i8259_query_intr();
 		i8259_ack_intr();
 		cpu.eip -=2;
