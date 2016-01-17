6961c40b1c48d680113857fd205f4d8dae901e0e
tracer-ics2015 <tracer@njuics.org>
2016-01-17 09:38:53 +0800

    >  compile NEMU 141220162 zhouyc Linux Debian 3.16.0-4-686-pae #1 SMP Debian 3.16.7-ckt11-1 (2015-05-24) i686 GNU/Linux  09:38:53 up 54 min,  6 users,  load average: 0.00, 0.01, 0.05 560502bdefdd10cb56166188b86959546207940

--- nemu/src/cpu/exec/transfer/lea-template.h
+++ nemu/src/cpu/exec/transfer/lea-template.h
@@ -1,15 +1,28 @@
 #include "cpu/exec/template-start.h"
+#include "cpu/decode/modrm.h"
 #define instr lea
 
 static void do_execute() {
 	if (DATA_BYTE==2){
-		OPERAND_W(op_dest,(short int)(op_src->imm&0xffff));
+		OPERAND_W(op_dest,(short int)(op_src->addr&0xffff));
 	}
 	else {
-		OPERAND_W(op_dest,op_src->simm);
+		OPERAND_W(op_dest,op_src->addr);
 	}
 	print_asm_template2();
 }
 
 make_instr_helper(rm2r)
+/*
+
+make_helper(concat(lea_rm2r_,SUFFIX)) {
+	ModR_M m;
+	m.val = instr_fetch(eip+1,1);
+	swaddr_t addr;
+	int len = read_ModR_M(eip+1, &addr, &addr);
+	REG(m.reg) = addr;
+	print_asm("lea"str(SUFFIX)"   %s,%%%s",ModR_M_asm,REG_NAME(m.reg));
+	return 1+len;
+}
+*/
 #include "cpu/exec/template-end.h"
