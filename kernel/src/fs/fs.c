80936a7d22b7b5800ec6688d63eec55c89f98906
tracer-ics2015 <tracer@njuics.org>
2016-01-18 21:43:02 +0800

    >  compile kernel 141220162 zhouyc Linux Debian 3.16.0-4-686-pae #1 SMP Debian 3.16.7-ckt11-1 (2015-05-24) i686 GNU/Linux  21:43:02 up  9:50,  7 users,  load average: 0.92, 0.96, 0.79 a08f1115205e5d84a078407af0dfa86335eecbb7

--- kernel/src/fs/fs.c
+++ kernel/src/fs/fs.c
@@ -94,6 +94,5 @@ int fs_read(int fd, void *buf, int len) {
 		len = file_table[fd-3].size-file_state[fd].offset-1;
 	ide_read(buf, file_table[fd-3].disk_offset+file_state[fd].offset, len);
 	file_state[fd].offset += len;
-	assert(0);
 	return len;
 }
